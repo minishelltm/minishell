f86d107d3be10dab98ce6f6ebb56ba57d1aee4fb
micael <mokili@DESKTOP-DR2K1EK.localdomain>
2025-10-11 20:13:35 +0200

    Ajout de la nouvelle version de ft_cd

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tonio <tonio@student.42.fr>                +#+  +:+       +#+        */
+/*   By: mmicael <mmicael@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 03:39:28 by tonio             #+#    #+#             */
-/*   Updated: 2025/10/10 10:46:07 by tonio            ###   ########.fr       */
+/*   Updated: 2025/10/11 19:55:07 by mmicael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,8 @@ int	ft_cd(char **args, t_node *env)
 {
 	int		retval;
 	char	*oldpwd;
+	char	*home;
+	char	*path;
 
 	retval = -404;
 	oldpwd = getcwd(NULL, 0);
@@ -26,15 +28,23 @@ int	ft_cd(char **args, t_node *env)
 		write(2, "cd: Too many arguments.\n", 23);
 		return (0);
 	}
-	if (args[1] != NULL && ft_strncmp(args[1], "-", 0) == 0)
-		retval = chdir(find_node(env, "OLDPWD=")->data.value);
 	if (args[1] == NULL)
-		retval = chdir("/home");
-	if (args[1] != NULL && ft_strncmp(args[1], "~", 0) == 0)
-		retval = chdir("/home");
-	if (args[1] != NULL && ft_strncmp(args[1], "/", 0) == 0)
+		retval = chdir(find_node(env, "HOME=")->data.value);
+	else if (ft_strncmp(args[1], "-", 0) == 0)
+		retval = chdir(find_node(env, "OLDPWD=")->data.value);
+	else if (ft_strncmp(args[1], "/", 0) == 0)
 		retval = chdir("/");
-	if (args[1] != NULL)
+	else if (args[1][0] == '~')
+	{
+		home = find_node(env, "HOME=")->data.value;
+		if (args[1][1] == '\0')
+			path = ft_strdup(home);
+		else
+			path = ft_strjoin(home, args[1] + 1);
+		retval = chdir(path);
+		free(path);
+	}
+	else
 		retval = chdir(args[1]);
 	if (retval == 0)
 		update_env("OLDPWD=", oldpwd, env);
