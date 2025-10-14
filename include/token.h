c6dbdfdc0c5c35212cfbb950f1ce02c1e7f90e30
Micael Micael <mmicael@f4r14s9.clusters.42paris.fr>
2025-10-14 18:56:46 +0200

    ajout de tokens.c

--- include/token.h
+++ include/token.h
@@ -1,26 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   tokens.h                                           :+:      :+:    :+:   */
+/*   token.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mmicael <mmicael@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 17:04:21 by mmicael           #+#    #+#             */
-/*   Updated: 2025/10/13 17:09:48 by mmicael          ###   ########.fr       */
+/*   Updated: 2025/10/14 18:54:22 by mmicael          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef TOKENS_H
 # define TOKENS_H
 
-typedef enum e_tokens
+typedef enum e_token_type
 {
 	WORD,
-	REDIR_IN,
-	REDIR_OUT,
-	HEREDOC,
-	APPEND,
-}					t_tokens;
+	REDIR_IN, // <
+	REDIR_OUT, // >
+	HEREDOC, // <<
+	APPEND, // >>
+}					t_token_type;
 
 typedef struct s_token
 {
---
+++ src/utils/tokens.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tokens.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mmicael <mmicael@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/14 18:49:34 by mmicael           #+#    #+#             */
+/*   Updated: 2025/10/14 18:53:49 by mmicael          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "token.h"
+#include "utils.h"
+
+t_token	*new_token(char *value, t_token_type type)
+{
+	t_token	*token;
+
+	token = malloc(sizeof(t_token));
+	if (!token)
+		return (NULL);
+	token->value = ft_strdup(value);
+	if (!token->value)
+	{
+		free(token);
+		return (NULL);
+	}
+	token->type = type;
+	token->next = NULL;
+	return (token);
+}
